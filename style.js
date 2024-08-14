5ad2482e4353a43cd9b78ee4176a9c47753137a2
shaikrubina2004 <168154033+shaikrubina2004@users.noreply.github.com>
2024-08-14 10:14:49 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,12 @@
+ocument.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
